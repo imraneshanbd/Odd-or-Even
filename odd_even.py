3a205d0b75e2597ac0ca7da01fca924e71c89c20
Md. Imran Hossain <69908749+imraneshanbd@users.noreply.github.com>
2021-09-15 12:08:08 +0600

    Create odd_even.py

---
+++ odd_even.py
@@ -0,0 +1,9 @@
+# Odd or Even
+# Don't Change the code below
+number = int(input("witch number do you want to change? "))
+# Don't Change the code Above
+
+if number % 2 == 0:
+    print("This number is an even number")
+else:
+    print("this number is an Odd number")
